a5519d35f86967c40a951f23bb17f43b417e8959
Michael Britt <mbritt3@greenriver.mail.edu>
2019-04-27 21:14:47 -0400

    updated js file to clear div on search submits

--- JS5/json-search.js
+++ JS5/json-search.js
@@ -1,20 +1,36 @@
-let searchArray =[];
-
+//event listener for button
 document.getElementById("button").addEventListener("click", ()=>{
-    var text = document.getElementById("textvalue").value.toLowerCase();
+    var text = document.getElementById("textvalue").value.toLowerCase();//grab text value and make lowercase
+    document.getElementById("searched").innerText = "";//rest div to nothing on each search
     $.getJSON("searchable.json", function (result) {
+        var notFound = true;//track wether value was found
+
+        //for each looping through array of json objects
         result.forEach((item)=>{
-            if(item.Name.toLowerCase().substring(0, text.length+1) == text)
+            var partOfName = item.Name.toLowerCase().substring(0, text.length);//takes item name and shrinks to size
+            //of text input
+
+            console.log(partOfName + " " + text);//if part of name and text input match display values
+            if( partOfName== text)
             {
                 objectToElement(item);
+                notFound=false;
             }
         })
+        if(notFound)// no names found
+        {
+            var p = document.createElement("p");
+            p.innerText="No matches found";
+            document.getElementById("searched").appendChild(p)
+        }
     });
 });
 
 
-
-
+/**
+ * Takes a object and parsed it out into a string of "Key:value"
+ * @param params an object provided
+ */
 function objectToElement(params)
 {
     for(var key in params)
